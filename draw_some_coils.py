585c85cdf3541e4c6afce073c840a07ed43a6f78
sarastimpson <stimpy90807@gmail.com>
2026-05-19 15:46:21 -0500

    sara changes

--- draw_some_coils.py
+++ draw_some_coils.py
@@ -11,22 +11,27 @@ from patch import *
 # Halliday & Resnick, 10th ed., question 29.83
 print("This next one is question 29.83")
 
-a = 0.08
+a = 20.0
+b = 10.0
 i = 10.0
-p0 = np.array([0,0,0])
-p1 = np.array([a,0,0])
-p2 = np.array([a,-a,0])
-p3 = np.array([0,-a,0])
+p0 = np.array([-a/2,b/2,0])
+p1 = np.array([a/2,b/2,0])
+p2 = np.array([a/2,-b/2,0])
+p3 = np.array([-a/2,-b/2,0])
 points = (p0,p1,p2,p3)
-r = np.array([a/4,-a/4,0])
+r = np.array([0,0,0])
 print(b_loop(i,points,r))
+
+
+btot = 2*mu_0 *i*sqrt(a**2 + b**2)/(pi*a*b)
+print(btot)
 
 ## repeat of 29.83:
 #thiscoil = coil(points,i)
 #print(thiscoil.b(r))
 #thiscoil.set_current(i/2.0)
 #print(thiscoil.b(r))
-#print(thiscoil.b_prime(a/4,-a/4,0))
+#print(thiscoil.b_prime(0,0,0))
 
 cs = coilset()
 cs.add_coil(points)
