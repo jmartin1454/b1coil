c74a051e94bca692dff97ee47298cac401851418
sarastimpson <stimpy90807@gmail.com>
2026-08-14 10:53:48 -0500

    .\shielding.py

--- shielding.py
+++ shielding.py
@@ -20,8 +20,8 @@ mu_r=1.09  #relative permeablility of material
 sigma=1.18*(10**7) #electrical conductivity of material in S/m
 r_0=0.3 #in m
 big_delta=0.002 #in m
-b=0.020 #outer radius in m
-a=0.018 #inner radius in m
+b=r_0+big_delta/2 #outer radius in m
+a=r_0-big_delta/2 #inner radius in m
 #big_delta=b-a   #wall thickness 
 
 
@@ -58,15 +58,14 @@ print("real z mag=", real_z_mag, "\n")
 omega=2*np.pi*1
 delta= np.sqrt(2/(omega*mu_0*mu_r*sigma)) 
 gamma=((1+1j)/delta)
-e=(1/(3*(b**3)*(gamma**2)*mu_r))
-h=((3*b)-big_delta)
-k=((a*b*(gamma**2)-1))
-l=(((a**2)*(b**2)*(gamma**2))+(b**2)-(a*big_delta))
-o=(gamma*big_delta)
-SE_ee=(np.abs((e*((2*big_delta*(mu_r**2))+mu_r*((a*b*h*\
-(gamma**2))-big_delta)+(big_delta*k))*np.cosh(o))+((e*\
-((gamma**2)*l)+(h*big_delta*mu_r*(gamma**2))+(2*k*\
-(mu_r**2))+mu_r+1)*np.sinh(o))))                
+print(gamma)
+e=1/(3*b**3*gamma**2*mu_r)
+h=3*b-big_delta
+k=a*b*gamma**2-1
+l=a**2*b**2*gamma**2+b**2-a*big_delta
+o=gamma*big_delta
+SE_ee=np.abs(e*(2*big_delta*mu_r**2+mu_r*(a*b*h*gamma**2-big_delta)+big_delta*k)*np.cosh(o)
+             +e/gamma*(gamma**2*l+h*big_delta*mu_r*gamma**2+2*k*mu_r**2+mu_r+1)*np.sinh(o))
 
 print("SE exact expression=", SE_ee)
 
@@ -88,7 +87,7 @@ ax.set_xscale('log')
 
 ax.set_xlabel("frequency in Hz")
 ax.set_ylabel("SE in dB")
-ax.set_ylim(0, 85)
+ax.set_ylim(-15, 85)
 ax.set_title("SE thick shield")
 
 ax.grid(True, which="both", ls="--", color='0.65')
@@ -108,10 +107,8 @@ h=((3*b)-big_delta)
 k=((a*b*(gamma**2)-1))
 l=(((a**2)*(b**2)*(gamma**2))+(b**2)-(a*big_delta))
 o=(gamma*big_delta)                    
-SE_ee=(np.abs((e*((2*big_delta*(mu_r**2))+mu_r*((a*b*h*\
-(gamma**2))-big_delta)+(big_delta*k))*np.cosh(o))+((e*\
-((gamma**2)*l)+(h*big_delta*mu_r*(gamma**2))+(2*k*\
-(mu_r**2))+mu_r+1)*np.sinh(o))))    
+SE_ee=np.abs(e*(2*big_delta*mu_r**2+mu_r*(a*b*h*gamma**2-big_delta)+big_delta*k)*np.cosh(o)
+             +e/gamma*(gamma**2*l+h*big_delta*mu_r*gamma**2+2*k*mu_r**2+mu_r+1)*np.sinh(o))   
                                                  
 SE_ee_db= 20*np.log10(SE_ee)  
 print()
@@ -123,7 +120,7 @@ ax.set_xscale('log')
 
 ax.set_xlabel("frequency in Hz")
 ax.set_ylabel("SE in dB")
-ax.set_ylim(0,85)
+ax.set_ylim(-15,85)
 ax.set_title("SE exact expression")
 
 ax.grid(True, which="both", ls="--", color='0.65')
@@ -146,10 +143,8 @@ h=((3*b)-big_delta)
 k=((a*b*(gamma**2)-1))
 l=(((a**2)*(b**2)*(gamma**2))+(b**2)-(a*big_delta))
 o=(gamma*big_delta)                    
-SE_ee=(np.abs((e*((2*big_delta*(mu_r**2))+mu_r*((a*b*h*\
-(gamma**2))-big_delta)+(big_delta*k))*np.cosh(o))+((e*\
-((gamma**2)*l)+(h*big_delta*mu_r*(gamma**2))+(2*k*\
-(mu_r**2))+mu_r+1)*np.sinh(o))))                                                 
+SE_ee=np.abs(e*(2*big_delta*mu_r**2+mu_r*(a*b*h*gamma**2-big_delta)+big_delta*k)*np.cosh(o)
+             +e/gamma*(gamma**2*l+h*big_delta*mu_r*gamma**2+2*k*mu_r**2+mu_r+1)*np.sinh(o))                                             
 SE_ee_db= 20*np.log10(SE_ee)  
 
 fig=plt.figure()
@@ -159,7 +154,7 @@ ax.plot(f, SE_ee_db, label="exact expression")
 ax.set_xscale('log')
 ax.set_xlabel("frequency in Hz")
 ax.set_ylabel("SE in dB")
-ax.set_ylim(0, 85)
+ax.set_ylim(-15, 85)
 ax.grid(True, which="both", ls="--", color='0.65')
 ax.set_title("SE_ee and SE_ts")
 ax.legend()
